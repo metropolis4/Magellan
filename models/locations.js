0caa2910a6175209cba0119b15e44137c4b0c8b2
Matt Rich <metropolis_4@hotmail.com>
2015-02-17 14:22:11 -0700

    added locations

---
+++ models/locations.js
@@ -0,0 +1,31 @@
+module.exports = {
+    Seville: 
+    {
+        info: 'Seville (/səˈvɪl/, Spanish: Sevilla [seˈβiʎa] ( listen), locally: [seˈβiʝa]) is the capital and largest city of the autonomous community of Andalusia and the province of Seville, Spain. It is situated on the plain of the River Guadalquivir. The inhabitants of the city are known as sevillanos (feminine form: sevillanas) or hispalenses, after the Roman name of the city, Hispalis. Seville has a municipal population of about 703,000 as of 2011, and a metropolitan population of about 1.5 million, making it the fourth-largest city in Spain and the 30th most populous municipality in the European Union. Its Old Town, the third largest in Europe with an area of 4 square kilometres (2 sq mi), contains three UNESCO World Heritage Sites: the Alcázar palace complex, the Cathedral and the General Archive of the Indies. The Seville harbour, located about 80 kilometres (50 miles) from the Atlantic Ocean, is the only river port in Spain. Seville is also the hottest major metropolitan area in Europe, with summer average high temperatures of above 35 °C.',
+        next: 'Canary-Islands'
+    },
+    'Canary-Islands': 
+    {
+        info: "The Canary Islands (English /kəˈnɛəri ˈaɪləndz/; Spanish: Islas Canarias [ˈizlas kaˈnaɾjas], locally: [ˈiɦlah kaˈnaɾjah]), also known as the Canaries (Spanish: Canarias), are a Spanish archipelago located just off the southwest coast of mainland Morocco, 100 kilometres (62 miles) west of the southern border of Morocco. The Canaries are one of Spain's 17 autonomous communities and are among the outermost region of the European Union proper. The islands include (from largest to smallest): Tenerife, Fuerteventura, Gran Canaria, Lanzarote, La Palma, La Gomera, El Hierro, La Graciosa, Alegranza, Isla de Lobos, Montaña Clara and Roque del Oeste.",
+        next: 'Cape-Verde'
+    },
+    'Cape-Verde': 
+    {
+        info: "Cape Verde /ˌkeɪp ˈvɜrd/ or Cabo Verde /kɑːboʊ ˈvɜrdeɪ/ (Portuguese: Cabo Verde, pronounced: [ˈkabu ˈveɾdɨ]), officially the Republic of Cabo Verde,[5] is an island country spanning an archipelago of 10 volcanic islands in the central Atlantic Ocean. Located 570 kilometres (350 miles) off the coast of Western Africa, the islands cover a combined area of slightly over 4,000 square kilometres (1,500 sq mi).",
+        next: 'Strait-of-Magellan'
+    },
+    'Strait-of-Magellan': 
+    {
+        info: "The Strait of Magellan (Spanish: Estrecho de Magallanes), also called the Straits of Magellan, is a navigable sea route separating mainland South America to the north and Tierra del Fuego to the south. The strait is the most important natural passage between the Atlantic and Pacific oceans but it is considered a difficult route to navigate because of the unpredictable winds and currents and the narrowness of the passage.",
+        next: 'Guam'
+    },
+    Guam: 
+    {
+        info: "Guam (Listeni/ˈɡwɑːm/ or /ˈɡwɒm/; Chamorro: Guåhån) is an organized, unincorporated territory of the United States in the western Pacific Ocean. It is one of five U.S. territories with an established civilian government.[4][5] Guam is listed as one of seventeen Non-Self-Governing Territories by the Special Committee on Decolonization of the United Nations.[6] The island's capital is Hagåtña (formerly rendered Agana). Guam is the largest and southernmost of the Mariana Islands.",
+        next: 'Philippines'
+    },
+    Philippines: 
+    {
+        info: "The Philippines (Listeni/ˈfɪlɨpiːnz/; Filipino: Pilipinas [ˌpɪlɪˈpinɐs]), officially known as the Republic of the Philippines (Filipino: Republika ng Pilipinas), is a sovereign island country in Southeast Asia situated in the western Pacific Ocean. It consists of 7,107 islands that are categorized broadly under three main geographical divisions: Luzon, Visayas, and Mindanao. Its capital city is Manila while its most populous city is Quezon City; both are part of Metro Manila."
+    }
+};
