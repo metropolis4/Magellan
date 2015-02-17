e2b50ee8cc0431e88b4e07f9c481bc601942aa07
Matt Rich <metropolis_4@hotmail.com>
2015-02-17 14:41:18 -0700

    Pagination

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
+var locations = require('./models/locations.js');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', indexController.index);
+// app.get('/', indexController.index);
+app.get('/:location?', function(req, res){
+    if(!req.params.location){req.params.location = 'Seville'}
+    res.render('index', {
+        location: req.params.location,
+        info: locations[req.params.location].info,
+        next: locations[req.params.location].next
+    });
+    // console.log(locations[req.params.location])
+});
 
 var server = app.listen(3269, function() {
 	console.log('Express server listening on port ' + server.address().port);
