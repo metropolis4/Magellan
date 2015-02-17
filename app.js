51003f6a445fd0e0b8e5369f27e961d6846f59dc
Matt Rich <metropolis_4@hotmail.com>
2015-02-17 14:54:55 -0700

    Refactoring

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 // var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
-var locations = require('./models/locations.js');
+
 
 var app = express();
 app.set('view engine', 'jade');
@@ -10,20 +10,7 @@ app.set('views', __dirname + '/views');
 // app.use(bodyParser.urlencoded({extended: false}));
 
 // app.get('/', indexController.index);
-app.get('/:location?', function(req, res){
-    if(!req.params.location){req.params.location = 'Seville'}
-    if(!locations[req.params.location]){
-        res.render('not-found', {
-            location: req.params.location
-        });
-    }
-    res.render('index', {
-        location: req.params.location,
-        info: locations[req.params.location].info,
-        next: locations[req.params.location].next
-    });
-    // console.log(locations[req.params.location])
-});
+app.get('/:location?', indexController.index);
 
 var server = app.listen(3269, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,23 @@
+var locations = require('../models/locations.js');
+
 var indexController = {
-	index: function(req, res) {
-		res.render('index');
-	}
+	index: function(req, res){
+        if(!req.params.location){req.params.location = 'Seville'}
+        if(!locations[req.params.location]){
+            res.render('not-found', {
+                location: req.params.location
+            });
+        }
+        res.render('index', {
+            location: req.params.location,
+            info: locations[req.params.location].info,
+            next: locations[req.params.location].next
+        });
+    },
+    notFound: function(req, res){
+
+    }
+
 };
 
 module.exports = indexController;
